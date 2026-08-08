e97bed45f7acbee523b4a6ba3e34661680bf7767
MeuMouse.com <contato@meumouse.com>
2026-08-08 19:54:34 -0300

    Set woocommerce tested up version 11.0.0

--- hubgo.php
+++ hubgo.php
@@ -8,7 +8,7 @@
  * Author URI:              https://meumouse.com/?utm_source=wordpress&utm_medium=hubgo&utm_campaign=plugins_list
  * Version:                 3.0.0
  * WC requires at least:    6.0.0
- * WC tested up to:         10.6.0
+ * WC tested up to:         11.0.0
  * Requires PHP:            7.4
  * Tested up to:            7.0.3
  * Text Domain:             hubgo
